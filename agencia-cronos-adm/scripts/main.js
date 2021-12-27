cee43facc8623157768c1a8cb37ee3848fb2d0f6
Gabriel Rodrigues <cgabrielrr@gmail.com>
2021-12-27 20:28:27 -0300

    Adicionado arquivos JavaScript ao projeto

---
+++ agencia-cronos-adm/scripts/main.js
@@ -0,0 +1,22 @@
+//objetos
+let servicos = [
+    {
+      nome: "Desenvolvimento",
+      descricao: "Cursinho de front-end",
+      id: 1,
+      img: "imagens/ilustra-web.png",
+    },
+    {
+      nome: "Marketing Digital",
+      descricao: "Marketeiros de Kottler",
+      id: 2,
+      img: "imagens/ilustra-marketing.png",
+    },
+    {
+      nome: "Consultoria UX",
+      descricao: "Vamo arruma esse layout/desing feio aí?",
+      id: 3,
+      img: "imagens/ilustra-ux.png",
+    },
+  ];
+  
