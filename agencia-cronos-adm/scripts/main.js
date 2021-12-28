a9a431c44e64ca3a350144f10fd937197d6d9f7a
WellingtonMax <wellington.psi@hotmail.com>
2021-12-27 23:15:25 -0300

    Adicionando funcionalidade de listagem de serviços

--- agencia-cronos-adm/scripts/main.js
+++ agencia-cronos-adm/scripts/main.js
@@ -1,22 +1,50 @@
 //objetos
 let servicos = [
-    {
-      nome: "Desenvolvimento",
-      descricao: "Cursinho de front-end",
-      id: 1,
-      img: "imagens/ilustra-web.png",
-    },
-    {
-      nome: "Marketing Digital",
-      descricao: "Marketeiros de Kottler",
-      id: 2,
-      img: "imagens/ilustra-marketing.png",
-    },
-    {
-      nome: "Consultoria UX",
-      descricao: "Vamo arruma esse layout/desing feio aí?",
-      id: 3,
-      img: "imagens/ilustra-ux.png",
-    },
-  ];
-  
+  {
+    nome: "Desenvolvimento Web",
+    descricao: "Cursinho de front-end",
+    id: 1,
+    img: "imagens/ilustra-web.png",
+  },
+  {
+    nome: "Marketing Digital",
+    descricao: "Marketeiros de Kottler",
+    id: 2,
+    img: "imagens/ilustra-marketing.png",
+  },
+  {
+    nome: "Consultoria UX",
+    descricao: "Vamo arruma esse layout/desing feio aí?",
+    id: 3,
+    img: "imagens/ilustra-ux.png",
+  },
+];
+
+// Criar Serviço = R do CRUD
+
+function listarServico() {
+  let imprimeServico = "";
+
+  for (let i = 0; i < servicos.length; i++) {
+    imprimeServico += `
+
+    <tr>
+    <td>${servicos[i].nome}</td>
+    <td><img src=${servicos[i].img} class="img-fluid" /></td>
+    <td>${servicos[i].descricao}</td>
+    <td>
+      <button class="btn btn-secondary m-1">editar</button>
+      <button class="btn btn-danger m-1">excluir</button>
+    </td>
+  </tr>
+  `;
+
+    // console.log(`Temos o serviço de ${servicos[i].nome}`)
+  }
+
+  let insereInforamacao = document.querySelector("tbody");
+
+  insereInforamacao.innerHTML = imprimeServico;
+}
+
+listarServico();
