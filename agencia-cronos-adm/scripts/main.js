537378faf2f6252aa0c16f037a63c1c44ae9e4d7
littate <pedrosilvestrejf@gmail.com>
2022-01-04 10:38:51 -0300

    edição de objetos no array

--- agencia-cronos-adm/scripts/main.js
+++ agencia-cronos-adm/scripts/main.js
@@ -1,37 +1,35 @@
-"use strict";
+'use strict';
 
 //objetos
 
-
-
 let servicos = [
   {
-    nome: "Desenvolvimento Web",
-    descricao: "Cursinho de front-end",
+    nome: 'Desenvolvimento Web',
+    descricao: 'Cursinho de front-end',
     id: 1,
-    img: "imagens/ilustra-web.png",
+    img: 'imagens/ilustra-web.png',
   },
   {
-    nome: "Marketing Digital",
-    descricao: "Marketeiros de Kottler",
+    nome: 'Marketing Digital',
+    descricao: 'Marketeiros de Kottler',
     id: 2,
-    img: "imagens/ilustra-marketing.png",
+    img: 'imagens/ilustra-marketing.png',
   },
   {
-    nome: "Consultoria UX",
-    descricao: "Vamo arruma esse layout/desing feio aí?",
+    nome: 'Consultoria UX',
+    descricao: 'Vamo arruma esse layout/desing feio aí?',
     id: 3,
-    img: "imagens/ilustra-ux.png",
+    img: 'imagens/ilustra-ux.png',
   },
 ];
 
 // Criar Serviço = R do CRUD
 
 function listarServico() {
-  let imprimeServico = "";
+  let imprimeServico = '';
 
   for (let i = 0; i < servicos.length; i++) {
-    imprimeServico +=`
+    imprimeServico += `
     
     <tr>
     <td>${servicos[i].nome}</td>
@@ -46,7 +44,7 @@ function listarServico() {
     // console.log(`Temos o serviço de ${servicos[i].nome}`)
   }
 
-  let insereInforamacao = document.querySelector("tbody");
+  let insereInforamacao = document.querySelector('tbody');
   insereInforamacao.innerHTML = imprimeServico;
 }
 
@@ -54,10 +52,10 @@ function listarServico() {
 let btnAdicaoDeServico = document.querySelector('[data-serviço="adc-servico"]');
 
 const adicionarServico = function () {
-  let novoServicoCadastrado = document.getElementById("input-nome");
-  let novoServicoDescricao = document.getElementById("input-descricao");
-  let novoServicoId = document.getElementById("input-id");
-  let novoServicoImg = document.getElementById("input-img");
+  let novoServicoCadastrado = document.getElementById('input-nome');
+  let novoServicoDescricao = document.getElementById('input-descricao');
+  let novoServicoId = document.getElementById('input-id');
+  let novoServicoImg = document.getElementById('input-img');
 
   servicos.push({
     nome: novoServicoCadastrado.value,
@@ -69,32 +67,40 @@ const adicionarServico = function () {
   listarServico();
 };
 
-btnAdicaoDeServico.addEventListener("click", adicionarServico);
+btnAdicaoDeServico.addEventListener('click', adicionarServico);
 
 listarServico();
 
 // Editando os serviços existentes
 
-
 function editarServico(id) {
- for (let i = 0; i < servicos.length; i++) {
-   if (servicos[i].id == id) {
-     document.querySelector('[data-input-nome]').value = servicos[i].nome
-     document.querySelector('[data-input-descricao]').value = servicos[i].descricao
-     document.querySelector('[data-input-id]').value = servicos[i].id
-     document.querySelector('[data-input-img]').value = servicos[i].img
-   }
- }
+  for (let i = 0; i < servicos.length; i++) {
+    if (servicos[i].id === id) {
+      document.querySelector('[data-edit-nome]').value = servicos[i].nome;
+      document.querySelector('[data-edit-descricao]').value = servicos[i].descricao;
+      document.querySelector('[data-edit-id]').value = servicos[i].id;
+      document.querySelector('[data-edit-img]').value = servicos[i].img;
+    }
+  }
 }
 
 function salvarEdicao() {
-  let nome = document.querySelector('[data-input-nome]')
-  let descricao = document.querySelector('[data-input-descricao]')
-  let id = document.querySelector('[data-input-id]')
-  let img = document.querySelector('[data-input-img]')
+  let editNome = document.querySelector('[data-edit-nome]');
+  let editDescricao = document.querySelector('[data-edit-descricao]');
+  let editId = document.querySelector('[data-edit-id]');
+  let editIMG = document.querySelector('[data-edit-img]');
 
-
-
-  
+  for (let i = 0; i < servicos.length; i++) {
+    if (editId.value == servicos[i].id) {
+      servicos[i] = {
+        nome: editNome.value,
+        descricao: editDescricao.value,
+        id: editId.value,
+        img: editIMG.value,
+      };
+      listarServico();
+    }
+  }
 }
 
+document.querySelector('[data-adc-servico]').addEventListener('click', salvarEdicao);
